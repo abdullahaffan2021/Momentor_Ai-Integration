0e39f8c8fb1bdb6fef38aa34616d720a1bad01fb
abdullahaffan2021 <146569120+abdullahaffan2021@users.noreply.github.com>
2026-01-27 17:08:07 +0530

    Delete mentors directory

--- mentors/src/main/java/com/momentor/mentors/repository/MeetingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.momentor.mentors.repository;
-
-import com.momentor.mentors.entity.Meeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MeetingRepository extends JpaRepository<Meeting,Long> {
-}
--- mentors/src/main/java/com/momentor/mentors/repository/userrepository.java
+++
@@ -1,7 +0,0 @@
-package com.momentor.mentors.repository;
-
-import com.momentor.mentors.entity.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userrepository extends JpaRepository<user,Long> {
-}
