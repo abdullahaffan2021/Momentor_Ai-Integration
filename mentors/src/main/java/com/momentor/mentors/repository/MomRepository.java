c76455683ab82601ded835a1ebbe7e7ef3406cb2
abdullahaffan2021 <146569120+abdullahaffan2021@users.noreply.github.com>
2026-01-28 16:52:49 +0530

    Delete mentors directory

--- mentors/src/main/java/com/momentor/mentors/repository/MomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.momentor.mentors.repository;
-
-import com.momentor.mentors.entity.Moms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MomRepository extends JpaRepository<Moms,Long>{
-
-}
