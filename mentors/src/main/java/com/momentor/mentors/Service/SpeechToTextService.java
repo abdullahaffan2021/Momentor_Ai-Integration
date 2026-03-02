f9d9a1cbc8c636ad621b86702cf5e0b876bb56b2
abdullahaffan2021 <146569120+abdullahaffan2021@users.noreply.github.com>
2026-03-02 14:47:58 +0530

    Delete mentors directory

--- mentors/src/main/java/com/momentor/mentors/Service/SpeechToTextService.java
+++
@@ -1,5 +0,0 @@
-package com.momentor.mentors.Service;
-import java.io.File;
-public interface SpeechToTextService { //It Act As a Socket in Microphone as Speech- Text Conversion(It is Must For Speech To Text Conversion.
-    String transcribe(File audioFile); //transcribe is expected outcome as Text
-}
--- mentors/src/main/java/com/momentor/mentors/repository/MeetingAudioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.momentor.mentors.repository;
-
-import com.momentor.mentors.entity.MeetingAudio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MeetingAudioRepository extends JpaRepository<MeetingAudio,Long> {
-}
