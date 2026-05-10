d5cb4f32ac0a391718712b26ab28122397dc9928
abdullahaffan2021 <146569120+abdullahaffan2021@users.noreply.github.com>
2026-05-10 21:34:24 +0530

    Delete mentors directory

--- mentors/src/main/java/com/momentor/mentors/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.momentor.mentors.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
